a21aa44ad32df9157146c3b6860940a99eba9028
Yash Khedekar <ykhedekar1107@gmail.com>
2026-06-18 20:26:13 +0530

    Implement application entry point and main menu

--- src/main.c
+++ src/main.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
-#include "../include/student.h"
+#include "../include/utils.h"
 
 int main(void)
 {
     int choice;
 
-    printf("==========================================\n");
-    printf("          CAMPUSSYNC-C\n");
-    printf("==========================================\n");
-    printf("Student Information System\n\n");
-
-    printf("1. Student Management\n");
-    printf("2. Academic Records\n");
-    printf("3. Search Student\n");
-    printf("4. Rank List\n");
-    printf("5. Statistics\n");
-    printf("0. Exit\n\n");
+    displayMainMenu();
 
     printf("Enter your choice: ");
     scanf("%d", &choice);
---
+++ src/utils.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "../include/utils.h"
+
+void displayMainMenu(void)
+{
+    printf("==========================================\n");
+    printf("          CAMPUSSYNC-C\n");
+    printf("==========================================\n");
+    printf("Student Information System\n\n");
+
+    printf("1. Student Management\n");
+    printf("2. Academic Records\n");
+    printf("3. Search Student\n");
+    printf("4. Rank List\n");
+    printf("5. Statistics\n");
+    printf("0. Exit\n\n");
+}
